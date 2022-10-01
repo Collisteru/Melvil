dc7b06da06e7c72744d5f84856b2d88f3a6198f5
Sean <sean14520@gmail.com>
2022-10-01 13:06:45 -0700

    Version 1.0.5

--- melvil_booklist/setup.py
+++ melvil_booklist/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='booklist-melvil',
-    version='1.0.4',
+    version='1.0.5',
     url='https://pypi.org/project/melvil-booklist/',
     author='Collisteru',
     author_email='collisteru@gmail.com',
